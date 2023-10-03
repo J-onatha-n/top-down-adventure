234903bb7ea6fa44b5fce7546cab74e16c568931
J-onatha-n <jonsanchez2304@gmail.com>
2023-10-03 14:18:21 -0400

    updated game

--- Top Down Adventure/Assets/Scripts/PlayerController.cs
+++ Top Down Adventure/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -46,4 +47,13 @@ public class PlayerController : MonoBehaviour
         
         transform.position = newPosition; 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Door"))
+        {
+            SceneManager.LoadScene("indoor");
+        }
+    }
+
 }
